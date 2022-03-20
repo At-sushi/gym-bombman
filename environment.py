42f8915c1f638f7d2013b89f85b0e6776501e408
At-sushi <dkahw210@kyoto.zaq.ne.jp>
2022-03-20 20:29:57 +0900

    implement environments

--- environment.py
+++ environment.py
@@ -1,11 +1,15 @@
-import sys
+import json, subprocess, sys
 
 import gym
 import gym.spaces
 
 
+PATH_SERVER = 'BombmanServer;BombmanServer\gson-2.3.1.jar;'
+CLASS_SERVER = 'BombmanServer'
+
 class BombmanEnv(gym.Env):
     def __init__(self):
+        self._server = subprocess.run(['java', '-classpath', PATH_SERVER, CLASS_SERVER])
 
     def _reset(self):
 
